abb22c6a07730f9cf19af6388fcc887acd019351
vikas swami <swamivikas2004@gmail.com>
2024-08-19 09:55:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "permalist",
-  password: "postgres@123",
+  database: "........",
+  password: ".......",
   port: 5432,
 });
 db.connect();
